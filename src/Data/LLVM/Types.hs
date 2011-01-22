ff33a4b8a6010f40a2c5eea9beb1b36b402cc30c
Tristan Ravitch <travitch@cs.wisc.edu>
2011-01-22 17:30:23 -0600

    Silence the orphan instance warnings for the Show instances of the LLVM data types.
    Technically these instances are declared in a file that doesn't contain
    the data declarations, but it is logically in the same package.  They
    are only separate to keep file sizes (and compile times) down.

--- src/Data/LLVM/Types.hs
+++ src/Data/LLVM/Types.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Data.LLVM.Types ( module Data.LLVM.Private.AttributeTypes
                        , module Data.LLVM.Private.ReferentialTypes
                        ) where
@@ -5,6 +6,17 @@ module Data.LLVM.Types ( module Data.LLVM.Private.AttributeTypes
 import Data.LLVM.Private.AttributeTypes
 import Data.LLVM.Private.Printers
 import Data.LLVM.Private.ReferentialTypes
+
+-- Technically, these are orphan instances.  But really, if you are
+-- using the public interface to this library they are not.  They are
+-- only defined outside of the module containing the data type
+-- declarations (Data.LLVM.Private.ReferentialTypes) to keep that
+-- module's compile time down.
+--
+-- I disable the warning since this seems reasonable.
+--
+-- This also would allow users to define their own show instances for
+-- these types if desired.
 
 instance Show Metadata where
   show = printMetadata
@@ -17,6 +29,4 @@ instance Show Value where
 
 instance Show Module where
   show = printModule
-
-
 
