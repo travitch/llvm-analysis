c67602e2c52830f6779b88ed53e73fdc9e1bed52
Tristan Ravitch <travitch@cs.wisc.edu>
2011-06-17 16:44:28 -0500

    Add a more reasonable set of inductive rules for the points-to analysis.
    Need to figure out how to seed points-to information, though.  Iterate over
    globals and parameters?

--- src/Data/LLVM/Analysis/PointsTo/Andersen.hs
+++ src/Data/LLVM/Analysis/PointsTo/Andersen.hs
@@ -1,4 +1,4 @@
-module Data.LLVM.Analysis.PointsTo.Andersen ( analysis ) where
+module Data.LLVM.Analysis.PointsTo.Andersen ( analysis, toLLVMValue ) where
 
 import Data.Hashable
 import Language.Datalog
@@ -26,48 +26,60 @@ analysis m = do
   values <- newDomain "values"
   index <- newDomain "integer"
 
-  -- A unary relation to mark some values (globals, the results of
-  -- allocators) as memory locations.
-  -- memLoc <- newRelation "memoryLocations" [ values ]
-
-  -- The value noted points to a memory location.  This works well
-  -- since all memory operations in LLVM work on pointers.
-  pointsToMemLoc <- newRelation "pointsToMemLoc" [ values ]
+  storeValToDst <- newRelation "storeValToDst" [ values, values ]
+  loadValAtLoc <- newRelation "loadValAtLoc" [ values, values ]
 
   -- pointsToEdge(a, b) is a fact that means that everything that a
   -- points to gets an edge to everything b points to.  This is equivalent
   -- to: Store b a
-  pointsToEdge <- newRelation "pointsToEdge" [ values, values ]
+--  pointsToEdge <- newRelation "pointsToEdge" [ values, values ]
 
   -- followEdges(dst, val) is a fact that means that the points-to set
   -- of dst is the set of values that can be pointed to by anything in
   -- the points-to set of val.
-  followEdges <- newRelation "followEdges" [ values, values ]
+  -- equivSet <- newRelation "equivSet" [ values, values ]
 
   pointsTo <- newRelation "pointsTo" [ values, values ]
 
-  [ a, b, c, d ] <- mapM newLogicVar [ "a", "b", "c", "d" ]
+  [ dst, val, loc, a, b, c ] <- mapM newLogicVar [ "dst", "val", "loc", "a", "b", "c" ]
 
-  extractFacts (pointsToMemLoc, pointsToEdge, followEdges) m
+  extractFacts (storeValToDst, loadValAtLoc, undefined) m
+
+  assertRule pointsTo [ a, b ] [ rel storeValToDst [ val, dst ]
+                               , rel pointsTo [ dst, a ]
+                               , rel pointsTo [ val, b ]
+                               ]
+  assertRule pointsTo [ a, b ] [ rel loadValAtLoc [ val, loc ]
+                               , rel pointsTo [ val, a ]
+                               , rel pointsTo [ loc, c ]
+                               , rel pointsTo [ c, b ]
+                               ]
 
   -- Simple case of p = &a
-  assertRule pointsTo [ a, b ] [ rel pointsToMemLoc [ b ]
-                               , rel pointsToEdge [ a, b ]
-                               ]
-  assertRule pointsTo [ a, b ] [ rel pointsTo [ b, c ]
-                               , rel pointsTo [ a, d ]
-                               , rel pointsTo [ d, c ]
-                               ]
-  [ va, vb, vc, vd ] <- mapM newLogicVar [ "va", "vb", "loadVal", "RefdVar" ]
-  assertRule pointsTo [ va, vb ] [ rel followEdges [ vc, vd ]
-                               , rel pointsToEdge [ va, vc ]
-                               , rel pointsTo [ vb, vd ]
-                               ]
+  -- assertRule pointsTo [ a, b ] [ rel pointsToEdge [ a, b ] ]
+  -- assertRule pointsTo [ a, b ] [ rel equivSet [ a, c ]
+  --                              , rel pointsTo [ c, b ]
+  --                              ]
+  -- assertRule pointsTo [ a, b ] [ rel p
+  -- assertRule pointsTo [ a, b ] [ rel pointsTo [ c, d ]
+  --                              , rel pointsTo [ c, b ]
+  --                              , rel pointsTo [ a, c ]
+  --                              ]
+  -- assertRule pointsTo [ a, b ] [ rel pointsTo [ b, c ]
+  --                              , rel pointsTo [ a, d ]
+  --                              , rel pointsTo [ d, c ]
+  --                              ]
+  -- [ va, vb, vc, vd ] <- mapM newLogicVar [ "va", "vb", "loadVal", "RefdVar" ]
+  -- assertRule pointsTo [ va, vb ] [ rel followEdges [ vc, vd ]
+  --                              , rel pointsToEdge [ va, vc ]
+  --                              , rel pointsTo [ vb, vd ]
+  --                              ]
   queryDatabase pointsTo [ a, b ]
 
 extractFacts :: (Relation, Relation, Relation) -> Module -> Datalog LogicValue ()
-extractFacts rels m =
+extractFacts rels m = do
   mapM_ (extractFunctionFacts rels) (moduleFunctions m)
+
 
 
 functionInstructions :: Value -> [Value]
@@ -81,20 +93,20 @@ extractFunctionFacts rels v =
   mapM_ (extractValueFacts rels) (functionInstructions v)
 
 extractValueFacts :: (Relation, Relation, Relation) -> Value -> Datalog LogicValue ()
-extractValueFacts (pointsToMemLoc, pointsToEdge, followEdges) v =
+extractValueFacts (storeValToDst, loadValAtLoc, _) v =
   case valueContent v of
     -- Stores must have the value be of a pointer type, otherwise it
     -- is a store of a scalar and not interesting
-    StoreInst _ val@Value { valueType = TypePointer _ } dst _ -> do
-      assertFact pointsToEdge [ LLVMValue dst, LLVMValue val ] `debug` "Asserted a storeInst fact"
-      case valueContent val of
-        LoadInst _ _ _ -> return ()
-        _ -> do
-          assertFact pointsToMemLoc [ LLVMValue val ] `debug` "Asserted a memLoc fact"
+    StoreInst _ val dst@Value { valueType = TypePointer _ } _ -> do
+      assertFact storeValToDst [ LLVMValue val, LLVMValue dst ]
+      -- case valueContent val of
+      --   LoadInst _ _ _ -> return ()
+      --   _ -> do
+      --     assertFact pointsToMemLoc [ LLVMValue val ] `debug` "Asserted a memLoc fact"
     -- Likewise, loads must be of type T** (at least), or it isn't
     -- interesting.
-    LoadInst _ val@Value { valueType = TypePointer (TypePointer _) } _ -> do
-      assertFact followEdges [ LLVMValue v, LLVMValue val ] `debug` "Asserted a loadInst fact"
+    LoadInst _ loc@Value { valueType = TypePointer _ } _ -> do
+      assertFact loadValAtLoc [ LLVMValue v, LLVMValue loc ]
     _ -> return ()
 
   -- A relation to mark formal parameters as such (so that they can be
--- tests/PointsTo.hs
+++ tests/PointsTo.hs
@@ -1,11 +1,23 @@
-import System.Environment ( getArgs )
+import Data.Maybe ( fromJust )
 import Language.Datalog
+import System.Environment ( getArgs )
+import Text.Printf
+
 import Data.LLVM.Analysis.PointsTo.Andersen
 import Data.LLVM
+import Data.LLVM.Types
 
 main :: IO ()
 main = do
   [ fname ] <- getArgs
   Right m <- parseLLVMBitcodeFile defaultParserOptions fname
   let res = evalDatalog (analysis m)
-  putStrLn $ show $ allResults res
+      allres = allResults res
+  mapM_ showPtPair allres
+
+tos = show . fromJust . valueName . toLLVMValue
+
+showPtPair [ v1, v2 ] = do
+  let v1' = tos v1
+      v2' = tos v2
+  printf "%s -> %s\n" v1' v2'
