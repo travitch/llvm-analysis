eea61a23f723c555732e6557c8aad758483858d9
Tristan Ravitch <travitch@cs.wisc.edu>
2011-07-29 15:53:07 -0500

    Add a convenience function to find the main function in a Module.
    This is handy for those analyses that require a list of entry points.

--- src/Data/LLVM.hs
+++ src/Data/LLVM.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- | This is the top-level LLVM IR module.  It provides functions to
 -- construct the IR from a bitcode file and some basic methods to
 -- inspect it.
@@ -11,10 +12,19 @@ module Data.LLVM (
   ParserOptions(..),
   PositionPrecision(..),
   -- * Inspect the IR
+  findMain,
   module Data.LLVM.Types
   ) where
+
+import Data.List ( find )
 
 import Data.LLVM.Private.Parser.Options
 import Data.LLVM.Private.Parser.Unmarshal
 import Data.LLVM.Types
 
+-- | Find the function named 'main' in the 'Module', if any.
+findMain :: Module -> Maybe Function
+findMain = find isMain . moduleDefinedFunctions
+  where
+    mainName = makeGlobalIdentifier "main"
+    isMain f = functionName f == mainName
