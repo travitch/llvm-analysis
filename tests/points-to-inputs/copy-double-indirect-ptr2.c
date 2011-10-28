84d490acba37e9fed93872c0ffa7e63060e3646b
Tristan Ravitch <travitch@cs.wisc.edu>
2011-10-28 11:54:14 -0500

    More tests

---
+++ tests/points-to-inputs/copy-double-indirect-ptr2.c
@@ -0,0 +1,31 @@
+/*
+  Note, this test is important because p picks up two edges from q at
+  the same step of the algorithm.  This gives us a chance to catch a
+  potential regression in graph construction (due to context
+  clobbering in FGL)
+ */
+int a;
+int b;
+int c;
+int d;
+
+int * p;
+int * q;
+int * r;
+int ** pq;
+int ** pp;
+
+void copyDoubleIndirectPtr()
+{
+  pp = pq;
+}
+
+void setup()
+{
+  q = &a;
+  q = &b;
+  r = &c;
+  pp = &p;
+  pq = &r;
+  pq = &q;
+}
