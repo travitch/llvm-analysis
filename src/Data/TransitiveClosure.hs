9b781860f5869eaac4c9e5686463ff50610a8550
Tristan Ravitch <travitch@cs.wisc.edu>
2011-12-19 08:35:04 -0600

    Use traversable...
    This is basically exactly what I want except there is no Traversable
    instance for Set (and it seems "impossible" to write).

--- src/Data/TransitiveClosure.hs
+++ src/Data/TransitiveClosure.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 -- | Module defines helpers for creating transitive-closure style algorithms
 module Data.TransitiveClosure ( markVisited ) where
 
-import Data.Foldable ( Foldable, foldMap )
+import Data.Foldable ( foldMap )
+import Data.Traversable ( Traversable, mapAccumR )
 import Data.Monoid
+import Data.Set ( Set )
 import qualified Data.Set as S
 
 -- | This is a combinator to allow easy implementation of
@@ -10,13 +13,16 @@ import qualified Data.Set as S
 -- seed value, apply @f@ to the seed and transitively to all of the
 -- results of that application, collecting all of the intermediate and
 -- final results into some Foldable container.
-markVisited :: (Ord a, Foldable t, Monoid (t a)) => (a -> t a) -> a -> t a
-markVisited f = mark' S.empty
+markVisited :: forall a t . (Ord a, Traversable t, Monoid (t a)) => (a -> t a) -> a -> t a
+markVisited f = snd . mark' S.empty
   where
+    mark' :: Set a -> a -> (Set a, t a)
     mark' visited a =
       case a `S.member` visited of
-        True -> mempty
+        True -> (visited, mempty)
         False ->
           let vis' = S.insert a visited
               newVals = f a
-          in newVals `mappend` foldMap (mark' vis') newVals
+              -- (vis'', transVals) = mapAccumR mark' vis' newVals
+              (vis'', transVals) = foldr mark' (vis', newVals) newVals
+          in (vis'', newVals `mappend` fold transVals)
