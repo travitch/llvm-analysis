5cc4d78dbf3689d3c5da1cece9bf51f6b5d7f5fe
Tristan Ravitch <travitch@cs.wisc.edu>
2012-02-29 15:55:37 -0600

    Add a parallel call graph traversal based on monad-par

--- src/Data/LLVM/Analysis/CallGraphSCCTraversal.hs
+++ src/Data/LLVM/Analysis/CallGraphSCCTraversal.hs
@@ -1,17 +1,27 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Data.LLVM.Analysis.CallGraphSCCTraversal (
   callGraphSCCTraversal,
-  basicCallGraphSCCTraversal
+  basicCallGraphSCCTraversal,
+  parallelCallGraphSCCTraversal
   ) where
 
-import Control.Monad ( foldM )
-import Data.Graph.Inductive
+import Control.DeepSeq
+import Control.Monad ( foldM, replicateM )
+import Control.Monad.Par
+import Data.Graph.Inductive hiding ( new )
+import Data.Map ( Map )
+import qualified Data.Map as M
+import Data.Monoid
 import FileLocation
 
 import Data.LLVM.Analysis.CallGraph
 import Data.LLVM.Types
 
 import Data.LLVM.Internal.Condense
+
+import Text.Printf
+import Debug.Trace
+debug' = flip trace
 
 -- | This is the high-level interface to the CallGraphSCCTraversal.
 -- It encapsualtes the logic to find a fixed-point for each SCC for
@@ -28,6 +38,22 @@ callGraphSCCTraversal cg analyzeFunction initialSummary =
     f [singleComponent] summ = analyzeFunction singleComponent summ
     -- Otherwise, we need to find a fixed-point of the summary over
     -- all of the functions in this SCC.
+    f sccComponents summ = do
+      newSummary <- foldM (flip analyzeFunction) summ sccComponents
+      case newSummary == summ of
+        True -> return summ
+        False -> f sccComponents newSummary
+
+parallelCallGraphSCCTraversal :: (NFData summary, Monad m, Monoid summary, Eq summary)
+                                 => CallGraph
+                                 -> (m summary -> summary)
+                                 -> (Function -> summary -> m summary)
+                                 -> summary
+                                 -> summary
+parallelCallGraphSCCTraversal cg unwrap analyzeFunction initialSummary =
+  parallelBasicCallGraphSCCTraversal cg unwrap f initialSummary
+  where
+    f [singleComponent] summ = analyzeFunction singleComponent summ
     f sccComponents summ = do
       newSummary <- foldM (flip analyzeFunction) summ sccComponents
       case newSummary == summ of
@@ -63,6 +89,64 @@ basicCallGraphSCCTraversal callgraph f seed =
     cg = condense g
     sccList = topsort' cg
     applyAnalysis summ component = f (map snd component) summ
+
+-- The components of each SCC are processed serially, but the SCCs
+-- themselves are processed in parallel using monad-par.  Each SCC
+-- gets forked off into its own monad-par thread and the dependencies
+-- are determined by the call graph.  summary will need to be an
+-- instance of monoid so that all dependencies can be combined purely.
+parallelBasicCallGraphSCCTraversal :: (NFData summary, Monad m, Monoid summary)
+                                      => CallGraph
+                                      -> (m summary -> summary)
+                                      -> ([Function] -> summary -> m summary)
+                                      -> summary
+                                      -> summary
+parallelBasicCallGraphSCCTraversal callgraph unwrap f seed = runPar $ do
+  -- Make an output variable for each SCC in the call graph.
+  outputVars <- replicateM (noNodes cg) new
+  let sccs = labNodes cg
+      varMap = M.fromList (zip (map fst sccs) outputVars)
+  mapM_ (forkSCC cg varMap unwrap f seed) sccs
+  let graphRoots = filter (isGraphRoot cg) (nodes cg)
+      rootVars = map (getDep varMap) graphRoots
+  finalVals <- mapM get rootVars
+  return $! mconcat finalVals
+  where
+    g = projectDefinedFunctions $ callGraphRepr callgraph
+    cg :: Gr [LNode Function] ()
+    cg = condense g
+
+isGraphRoot :: (Graph gr) => gr a b -> Node -> Bool
+isGraphRoot cg = null . pre cg
+
+getDep :: Map Node c -> Node -> c
+getDep m n =
+  case M.lookup n m of
+    Nothing -> $err' ("Missing expected output var for node: " ++ show n)
+    Just v -> v
+
+forkSCC :: (NFData summary, Monad m, Monoid summary)
+           => Gr [LNode Function] ()
+           -> Map Node (IVar summary)
+           -> (m summary -> summary)
+           -> ([Function] -> summary -> m summary)
+           -> summary
+           -> LNode [LNode Function]
+           -> Par ()
+forkSCC cg varMap unwrap f val0 (nid, component) = fork $ do
+  -- SCCs can contain self-loops in the condensed call graph, so
+  -- remove those self loops here so we don't block the entire
+  -- parallel computation with a thread waiting on itself.
+  let deps = filter (/=nid) $ suc cg nid
+      depVars = map (getDep varMap) deps
+  depVals <- mapM get depVars
+  let seed = case null deps of
+        True -> val0
+        False -> mconcat depVals
+      sccSummary = f (map snd component) seed
+      sccVar = getDep varMap nid
+  put sccVar (unwrap sccSummary)
+
 
 projectDefinedFunctions :: Gr CallNode b -> Gr Function b
 projectDefinedFunctions g = nmap unwrap g'
