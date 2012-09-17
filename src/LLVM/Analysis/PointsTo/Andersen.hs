6ace08f93584413c088371d781193b3c3dfaa149
Tristan Ravitch <travitch@cs.wisc.edu>
2012-09-17 17:09:33 -0500

    Revive the andersens analysis
    This time the implementation is based on an actual set constraints
    solver (which is external and can be optimized separately).  Several
    tests pass and the basic operation seems to be in place.  It needs
    handling of phi nodes and function calls, but other than that seems
    to be well on its way.
    
    Field-sensitivity will come later; field sensitivity may be limited in
    some ways.  Additionally, the analysis will have special cases to
    handle C++ virtual function calls.

--- src/LLVM/Analysis/PointsTo/Andersen.hs
+++ src/LLVM/Analysis/PointsTo/Andersen.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, OverloadedStrings, ScopedTypeVariables, BangPatterns #-}
+{-# LANGUAGE ViewPatterns, DeriveDataTypeable #-}
 -- | This is a simple implementation of Andersen's points-to analysis.
 --
 -- TODO:
@@ -24,10 +24,138 @@ module LLVM.Analysis.PointsTo.Andersen (
   -- * Constructor
   runPointsToAnalysis,
   -- * Debugging aids
-  savePointsToGraph,
-  viewPointsToGraph,
+  -- savePointsToGraph,
+  -- viewPointsToGraph,
   ) where
 
+import Control.Exception
+import Control.Monad.State.Strict
+import Data.Typeable
+
+import LLVM.Analysis
+import LLVM.Analysis.PointsTo
+
+import Constraints.Set.Solver
+
+import Debug.Trace
+debug = flip trace
+
+-- A monad to manage fresh variable generation
+data ConstraintState = ConstraintState { freshIdSrc :: !Int  }
+type ConstraintGen = State ConstraintState
+
+freshVariable :: ConstraintGen SetExp
+freshVariable = do
+  s <- get
+  let vid = freshIdSrc s
+      v = Fresh vid
+  put $ s { freshIdSrc = vid + 1 }
+  return $! setVariable v
+
+
+data Constructor = Ref
+                 | Atom !Value
+                 deriving (Eq, Ord, Show, Typeable)
+data Var = Fresh !Int
+         | LocationSet !Value -- The X_{l_i} variables
+         | LoadedLocation !Instruction
+         deriving (Eq, Ord, Show, Typeable)
+
+type SetExp = SetExpression Var Constructor
+data Andersen = Andersen (SolvedSystem Var Constructor)
+
+instance PointsToAnalysis Andersen where
+  mayAlias = andersenMayAlias
+  pointsTo = andersenPointsTo
+
+andersenMayAlias :: Andersen -> Value -> Value -> Bool
+andersenMayAlias _ _ _ = True
+
+andersenPointsTo :: Andersen -> Value -> [Value]
+andersenPointsTo (Andersen ss) v =
+  either fromError (map fromLocation) (leastSolution ss var)
+  where
+    var = LocationSet v
+    fromError :: ConstraintError Var Constructor -> [Value]
+    fromError = const []
+    fromLocation :: SetExp -> Value
+    fromLocation (ConstructedTerm Ref _ [ConstructedTerm (Atom val) _ _, _, _]) = val
+
+runPointsToAnalysis :: Module -> Andersen
+runPointsToAnalysis m = evalState (pta m) (ConstraintState 0)
+
+-- | Generate constraints and solve the system.  Any system we
+-- generate should be solvable if we are generating the correct
+-- constraints, so convert solving failures into an IO exception.
+pta :: Module -> ConstraintGen Andersen
+pta m = do
+  initConstraints <- foldM globalInitializerConstraints [] (moduleGlobalVariables m)
+  funcConstraints <- foldM functionConstraints [] (moduleDefinedFunctions m)
+  let is = initConstraints ++ funcConstraints
+      cs = constraintSystem is -- `debug` (unlines $ map show is)
+      sol = either throwErr id (solveSystem cs)
+  return $! Andersen sol
+  where
+    loadVar ldInst = setVariable (LoadedLocation ldInst)
+    ref = term Ref [Covariant, Covariant, Contravariant]
+    loc val =
+      let var = case valueContent' val of
+            InstructionC i@LoadInst {} -> loadVar i
+            _ -> setVariable (LocationSet val)
+      in ref [ atom (Atom val), var, var ]
+
+    -- Have to be careful handling phi nodes - those will actually need to
+    -- generate many constraints, and the rule for each one can generate a
+    -- new set of assignments.
+    setExpFor v = case valueContent' v of
+      InstructionC i@LoadInst {} -> loadVar i
+      _ -> loc v
+
+    -- FIXME This probably needs to use the type of the initializer to
+    -- determine if the initializer is a copy of another global or an
+    -- address to a specific location
+    globalInitializerConstraints acc global =
+      case globalVariableInitializer global of
+        Nothing -> return acc
+        Just (valueContent -> ConstantC _) -> return acc
+        Just i -> do
+          f1 <- freshVariable
+          f2 <- freshVariable
+          let c1 = loc (toValue global) <=! ref [ universalSet, universalSet, f1 ]
+              c2 = ref [ emptySet, loc i, emptySet ] <=! ref [ universalSet, f2, emptySet ]
+              c3 = f2 <=! f1
+          return $ c1 : c2 : c3 : acc
+
+    functionConstraints acc = foldM instructionConstraints acc . functionInstructions
+    instructionConstraints acc i =
+      case i of
+        LoadInst { loadAddress = la } -> do
+          let c = loc la <=! ref [ universalSet, loadVar i, emptySet ] -- `debug`
+                    -- ("Load constraint: " ++ show ((loadVar i) :: SetExpression Var Constructor))
+          return $ c : acc
+        StoreInst { storeAddress = sa, storeValue = sv } -> do
+          f1 <- freshVariable
+          f2 <- freshVariable
+          let c1 = setExpFor sa <=! ref [ universalSet, universalSet, f1 ]
+              c2 = ref [ emptySet, setExpFor sv, emptySet ] <=! ref [ universalSet, f2, emptySet ] -- `debug` show (loc sv)
+              c3 = f2 <=! f1
+          return $ c1 : c2 : c3 : acc
+        _ -> return acc
+
+          -- let c1 = setExpFor sa <=! ref [ universalSet, universalSet, f1 ]
+          --     c2 = ref [ emptySet, loc sv, emptySet ] <=! ref [ universalSet, f2, emptySet ] -- `debug` show (loc sv)
+          --     c3 = f2 <=! f1
+
+
+-- Helpers
+
+
+throwErr :: ConstraintError Var Constructor -> SolvedSystem Var Constructor
+throwErr = throw
+
+
+
+{-
 import Control.Monad ( forM_, unless )
 import Control.Monad.ST
 import Data.ByteString.Char8 ( isPrefixOf )
@@ -734,4 +862,6 @@ showNodeLabels tag g = (tag'++) . unlines . map toS
     tag' = tag ++ "\n"
     toS n = let Just lbl = lab g n
             in "  " ++ show lbl
+-}
+
 -}
--- tests/AndersenTest.hs
+++ tests/AndersenTest.hs
@@ -1,34 +1,39 @@
+module Main ( main ) where
+
 import Data.Map ( Map )
 import Data.Set ( Set )
 import qualified Data.Map as M
 import qualified Data.Set as S
-import Data.Maybe ( fromJust )
+import Data.Monoid
+import System.Environment ( getArgs, withArgs )
 import System.FilePath
 import Test.HUnit ( assertEqual )
 
 import LLVM.Analysis
-import LLVM.Analysis.PointsTo.AllocatorProfile
+-- import LLVM.Analysis.PointsTo.AllocatorProfile
 import LLVM.Analysis.PointsTo.Andersen
 import LLVM.Analysis.PointsTo
 import LLVM.Analysis.Util.Testing
 import LLVM.Parse
 
-ptPattern = "tests/points-to-inputs/*.c"
-expectedMapper = flip replaceExtension ".expected-andersen"
-bcParser = parseLLVMFile defaultParserOptions
+import Debug.Trace
+debug = flip trace
 
--- extractSummary :: Module -> ExpectedResult
-extractSummary m = foldr addInfo M.empty ptrs
+type ExpectedResult = Map String (Set String)
+
+extractSummary :: Module -> ExpectedResult
+extractSummary m = foldr addInfo mempty ptrs
   where
-    pta = runPointsToAnalysis [standardCProfile] m
-    ptrs = map Value (globalPointerVariables m) ++ map Value (functionPointerParameters m)
-    addInfo v r = case S.null vals of
-      True -> r
-      False -> M.insert (show $ fromJust $ valueName v) vals r
-      where
-        vals = case pointsTo pta v of
-          PTSet s -> S.map show s
-          UniversalSet s -> "unknown" `S.insert` S.map show s
+    pta = runPointsToAnalysis m
+    ptrs = map toValue (globalPointerVariables m) -- ++ map Value (functionPointerParameters m)
+    addInfo v r =
+      let vals = pointsTo pta v
+          name = maybe "???" show (valueName v)
+      in case null vals of
+        True -> r
+        False ->
+          let targets = map (maybe "??" show . valueName) vals -- `debug` show vals
+          in M.insert name (S.fromList targets) r
 
 isPointerType t = case t of
   TypePointer _ _ -> True
@@ -45,12 +50,21 @@ functionPointerParameters m = concatMap pointerParams (moduleDefinedFunctions m)
   where
     pointerParams = filter isPointer . functionParameters
 
-testDescriptors = [ TestDescriptor { testPattern = ptPattern
-                                   , testExpectedMapping = expectedMapper
-                                   , testResultBuilder = extractSummary
-                                   , testResultComparator = assertEqual
-                                   }
-                  ]
-
 main :: IO ()
-main = testAgainstExpected [] bcParser testDescriptors
+main = do
+  args <- getArgs
+  let pattern = case args of
+        [] -> "tests/points-to-inputs/*.c"
+        [infile] -> infile
+        _ -> error "Only one argument allowed"
+      testDescriptors = [ TestDescriptor { testPattern = pattern
+                                         , testExpectedMapping = expectedMapper
+                                         , testResultBuilder = extractSummary
+                                         , testResultComparator = assertEqual
+                                         }
+                        ]
+  withArgs [] $ testAgainstExpected opts parser testDescriptors
+  where
+    opts = [ "-mem2reg", "-basicaa", "-gvn" ]
+    parser = parseLLVMFile defaultParserOptions
+    expectedMapper = (<.> "expected-andersen")
